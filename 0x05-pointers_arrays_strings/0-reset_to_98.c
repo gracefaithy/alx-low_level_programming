c831399288d51dced289f2e61e418c2d8e6854c2
gracefaithy <gracefaithokechukwu@gmail.com>
2024-02-27 02:21:54 +0100

    pointers task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
+
 /**
- * code that reset an integer
+ * reset_to_98 - resets value of pointer to 98
+ * @n: pointer to reset to 98
  *
- * Return ; Always o.
- *
+ * Return: void
  */
 void reset_to_98(int *n)
 {
-	*n = 98
+	*n = 98;
 }
 
